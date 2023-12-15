c16725193d7f8d059bcac7aaf1dd15941dea5515
root <root@0xHammam>
2023-12-15 19:28:07 +0200

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/*
+/**
  * main - Entry point of the program
+ * 
+ * Description: This program generates a random number and prints whether
+ * the number is positive, zero, or negative.
  *
  * Return: Always 0 (Success)
  */
