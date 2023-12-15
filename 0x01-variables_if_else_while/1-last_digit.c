018f3b6e36e4ea22210791e8203d368ffccda107
root <root@0xHammam>
2023-12-15 19:51:24 +0200

    f

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - Entry point of the program
- * 
+ *
  * Description: This program generates a random number and prints whether
  * the number is positive, zero, or negative.
  *
@@ -15,7 +15,7 @@ int n;
 int l;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-l=n%10;
+l = n % 10;
 if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, l);
