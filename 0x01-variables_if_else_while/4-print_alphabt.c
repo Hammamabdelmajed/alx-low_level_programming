96302b449b39831b8aee2c931c640de49502d0a5
root <root@0xHammam>
2023-12-15 23:26:54 +0200

    g

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number and prints whether
+ * the number is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+putchar('\n');
+return (0);
+}
