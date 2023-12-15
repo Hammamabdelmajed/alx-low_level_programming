f57b42b621e5d27eae62e68e828c81263ae05e76
root <root@0xHammam>
2023-12-15 18:57:54 +0200

    g

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,13 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* The main function should have a return type of int.
+ * The function should not take any parameters, so its declaration should be int main(void).
+ * The opening curly brace { should be on the same line as the function declaration.
+ * The closing curly brace } should be on a new line, aligned with the main function declaration.
+ * Each statement within the main function should be indented with four spaces.
+ * The return statement should include a return value (usually 0 for success).
+ * Here's an example of how the main funct */
 int main(void)
 {
 int n;
