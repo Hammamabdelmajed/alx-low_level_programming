86baf645ade93c38dea8193fc6311bd3242b5b3c
root <root@0xHammam>
2023-12-15 19:06:19 +0200

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 
-/* The main function should have a return type of int.
- * The function should not take any parameters, so its declaration should be int main(void).
- * The opening curly brace { should be on the same line as the function declaration.
- * The closing curly brace } should be on a new line, aligned with the main function declaration.
- * Each statement within the main function should be indented with four spaces.
- * The return statement should include a return value (usually 0 for success).
- * Here's an example of how the main funct */
+/*
+ * File: positive_or_negative.c
+ * Auth: Hammam
+ * Desc: positive or negative number
+ */
 int main(void)
 {
 int n;
@@ -17,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is positive", n);
+printf(" %d is positive", n);
 }
 else if (n == 0)
 {
-printf("%d is zero", n);
+printf(" %d is zero", n);
 }
 else
 {
-printf("%d is negative", n);
+printf(" %d is negative", n);
 }
 return (0);
 }
