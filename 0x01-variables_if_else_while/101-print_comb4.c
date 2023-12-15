6044c06ae8c1350a0f0c7f81b47384ed2cb9d982
root <root@0xHammam>
2023-12-16 01:33:54 +0200

    f

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number and prints whether
+ * the number is positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
