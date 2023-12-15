ee652344f00591e01ec36e1b551a49a733498244
root <root@0xHammam>
2023-12-15 19:08:55 +0200

    f

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /* more headers goes there */
 
 /*
- * File: positive_or_negative.c
- * Auth: Hammam
- * Desc: positive or negative number
+ * main - Entry point of the program
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -15,15 +15,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf(" %d is positive", n);
+printf("%d is positive ", n);
 }
 else if (n == 0)
 {
-printf(" %d is zero", n);
+printf("%d is zero ", n);
 }
 else
 {
-printf(" %d is negative", n);
+printf("%d is negative ", n);
 }
 return (0);
 }
