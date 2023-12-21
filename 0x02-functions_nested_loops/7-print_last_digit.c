3da3dfdf7852fdea245a0bdbdf831db8d7615525
root <root@0xHammam>
2023-12-21 04:19:23 +0200

    f

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,7 @@
  * print_last_digit - prints the last digit of a number
  * @n: the number to get the last digit of
  *
- * Description: This function computes the last digit of an integer `n` 
+ * Description: This function computes the last digit of an integer `n`
  * by taking the modulus of `n` by 10.
  * The function then prints this last digit and returns it.
  *
@@ -13,10 +13,10 @@
 int print_last_digit(int n)
 {
 int last_digit = n % 10;
-if(last_digit < 0)
+if (last_digit < 0)
 {
 last_digit = -last_digit;
 }
-putchar('0' + last_digit);
-return last_digit;
+_putchar('0' + last_digit);
+return (last_digit);
 }
