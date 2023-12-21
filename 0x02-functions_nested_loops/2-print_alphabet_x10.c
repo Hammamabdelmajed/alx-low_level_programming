ced57876bbc3380d025c0b33815103317ff34a55
root <root@0xHammam>
2023-12-21 02:30:21 +0200

    f

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ * print alphabet in lowercase
+ * Description: this function uses a loop to interate through ASCII
+ * values of the lowercase.
+ * alphabet
+ * Return: this function does not return a value
+ */
+void print_alphabet_x10(void)
+{
+int i = 1;	
+while(i<=10)
+{	
+char letter;
+for (letter = 'a'; letter <= 'z' ; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+i++;
+}
+}
