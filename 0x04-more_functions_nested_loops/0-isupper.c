6aa0368d91c89a3602dddb5cf4e91093c2166448
root <root@0xHammam>
2023-12-21 19:38:16 +0200

    f

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _islupper - checks for upper case characte
+* @c: the character to check
+* Description: This function checks if the ASCII value of `c` falls within the
+* range of uppercase letters (from 'A' to 'Z'). If it does, the function
+* returns 1, indicating that the character is uppercase. Otherwise,
+* it returns 0.
+* This function only works for ASCII characters.
+* Return: Returns 1 if c is uppercase, 0 otherwise   
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int r);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a , int b);
+void print_to_98(int n);
+void print_number(int n);
+void print_number_rec(int c);
+int _isupper(int c);
+#endif /* MAIN_H */
