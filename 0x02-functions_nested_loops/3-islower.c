1535f04ea7d31ec96c52276aef70ee2d254b24f7
root <root@0xHammam>
2023-12-21 03:13:32 +0200

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,8 @@
  *
  * Description: This function checks if the ASCII value of `c` falls within the
  * range of lowercase letters (from 'a' to 'z'). If it does, the function
- * returns 1, indicating that the character is lowercase. Otherwise, it returns 0.
+ * returns 1, indicating that the character is lowercase. Otherwise,
+ * it returns 0.
  * This function only works for ASCII characters.
  *
  * Return: Returns 1 if c is lowercase, 0 otherwise
