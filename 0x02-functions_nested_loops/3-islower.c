5c3c1fbd5be82e13fbb929a33ab118ec8a78bb79
root <root@0xHammam>
2023-12-21 03:12:03 +0200

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower - checks if the letter is lowercase or not
- * return 1 if yes and 0 if not
- * Description: this function uses a loop to interate through ASCII
- * values of the lowercase. using c as parameter
- * will be used as variable that could be changed anytime
- * Return: this function returns 0 or 1
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ *
+ * Description: This function checks if the ASCII value of `c` falls within the
+ * range of lowercase letters (from 'a' to 'z'). If it does, the function
+ * returns 1, indicating that the character is lowercase. Otherwise, it returns 0.
+ * This function only works for ASCII characters.
+ *
+ * Return: Returns 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
