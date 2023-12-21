acc3e3b518def6ee81657d911fe34a43f2c5f853
root <root@0xHammam>
2023-12-21 02:53:52 +0200

    f

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower - Prints the alphabet in 10 times lowercase
+ * _islower - Prints the alphabet in times lowercase
  * print alphabet in lowercase
  * Description: this function uses a loop to interate through ASCII
  * values of the lowercase.
