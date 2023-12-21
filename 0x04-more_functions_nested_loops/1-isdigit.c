8a40d5c7c4ce078065bbca25a3cf8e710decfe44
root <root@0xHammam>
2023-12-21 19:52:14 +0200

    f

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,8 @@
 /**
  * _isdigit - checks for parameter if it is digit
  * @c: the character to check
- * Description: This function checks if the entered parameter `c` falls within the
+ * Description: This function checks if
+ * the entered parameter `c` falls within the
  * range of digits (from '0' to '9'). If it does, the function
  * returns 1, indicating that the parameter is a digit. Otherwise,
  * it returns 0.
@@ -10,7 +11,7 @@
  */
 int _isdigit(int c)
 {
-if ( c >= '0' && c <= '9')
+if (c >= '0' && c <= '9')
 {
 return (1);
 }
