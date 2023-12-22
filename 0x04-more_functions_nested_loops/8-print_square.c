57f2677b8d9e5eae0317014480a9e8d5c2744581
root <root@0xHammam>
2023-12-23 01:05:17 +0200

    m

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,7 @@ void print_square(int size)
 {
 int i;
 int j;
-if (size < 0)
-{
-_putchar('\n');
-}
-else
+if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
@@ -22,7 +18,10 @@ for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
-_putchar('\n');
+_putchar
 }
+else
+{
+_putchar('\n');
 }
 }
