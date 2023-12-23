ed84411279498d70c81b74127a6234c60cdf1314
root <root@0xHammam>
2023-12-23 02:43:03 +0200

    f

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,7 +2,7 @@
 /**
  *  * print_number - Prints an integer.
  *   * @n: The integer to be printed.
- *    */
+ */
 void print_number(int n)
 {
 /* Handle negative numbers */
@@ -14,7 +14,7 @@ n = -n;
 /* Recursively print digits from left to right */
 if (n / 10)
 {
- print_number(n / 10);
+print_number(n / 10);
 }
 /* Print last digit */
 _putchar(n % 10 + '0');
