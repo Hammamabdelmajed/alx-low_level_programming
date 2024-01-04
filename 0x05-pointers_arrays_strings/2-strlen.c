692588204994ec56dacada3334fcbe3af13bcc40
hammam <hammamabdelmajed@gmail.com>
2024-01-05 00:26:25 +0200

    f

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: a pointer to a string
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+int len;
+len = strlen(s);
+return (len);
+}
