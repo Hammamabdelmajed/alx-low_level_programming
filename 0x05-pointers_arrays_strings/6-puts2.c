2a46c97d146f8c5be4777c0e603952f7f51304bb
hammam <hammamabdelmajed@gmail.com>
2024-01-05 02:00:42 +0200

    f

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * pruts_2 - Prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @s: The string to be printed.
+ * Return: void.
+ */
+void puts2(char *s)
+{
+int i;
+int length = strlen(s);
+for (i = 0; i < length; i += 2)
+{
+printf("%c", s[i]);
+}
+printf("\n");
+}
