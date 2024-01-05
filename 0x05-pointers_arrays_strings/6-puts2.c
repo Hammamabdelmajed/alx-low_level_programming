8683c76bc4f1b18f4994985d780de8b0e9a787ea
hammam <hammamabdelmajed@gmail.com>
2024-01-05 02:11:40 +0200

    f

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "main.h"
 /**
- * pruts_2 - Prints every other character of a string,
+ * pruts2 - Prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @s: The string to be printed.
  * Return: void.
