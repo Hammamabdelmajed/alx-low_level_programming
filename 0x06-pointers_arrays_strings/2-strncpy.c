fd316c2974f396787595a293a8ea5ed8cf2ecf28
hammam <hammamabdelmajed@gmail.com>
2024-01-05 03:24:05 +0200

    f

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strncpy - Copies up to n characters from the string pointed to by @src to @dest.
+ * _strncpy - Copies up to n characters from the
+ *  string pointed to by @src to @dest.
  * @dest: A pointer to the first character of the destination string.
  * @src: A pointer to the first character of the source string.
  * @n: Maximum number of characters to be copied from src.
@@ -10,6 +11,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-strncpy(dest, src , n);
+strncpy(dest, src, n);
 return (dest);
 }
