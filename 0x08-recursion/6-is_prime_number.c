486c49169f3efffdf33516109b1e9e36734b0063
hammam <hammamabdelmajed@gmail.com>
2024-01-12 02:14:56 +0200

    f

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,37 +1,55 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *  * is_prime_number - checks if a number is prime
+ *  * is_prime_number_recursion - checks if a number is prime using recursion
  *   * @n: the number to check
- *    *
- *     * Description: This function checks
- *     if a number is prime. If the number is prime,
- *      * it returns 1. Otherwise, it returns 0.
- *       *
- *        * Return: 1 if n is a prime number, 0 otherwise
+ *    * @i: the current divisor to check
+ *     *
+ *      * Description: This function checks
+ *      if a number is prime using recursion. If the number is prime,
+ *       * it returns 1. Otherwise, it returns 0.
+ *        *
+ *         * Return: 1 if n is a prime number, 0 otherwise
  */
-int is_prime_number(int n)
+int is_prime_number_recursion(int n, int i)
 {
-int i = 5;
 if (n <= 1)
 {
 return (0);
 }
-else if (n <= 3)
+else if (n == i)
 {
 return (1);
 }
-else if (n % 2 == 0 || n % 3 == 0)
+else if (n % i == 0)
 {
 return (0);
 }
-while (i * i <= n)
+else
 {
-if (n % i == 0 || n % (i + 2) == 0)
+return (is_prime_number_recursion(n, i + 1));
+}
+}
+/**
+ *  * is_prime_number - a wrapper function for is_prime_number_recursion
+ *   * @n: the number to check
+ *    *
+ *     * Description: This function checks
+ *     if n is negative before calling is_prime_number_recursion.
+ *      * If n is negative, the function returns 0.
+ *      Otherwise, it calls is_prime_number_recursion with
+ *       * an initial divisor of 2.
+ *        *
+ *         * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n < 0)
 {
 return (0);
 }
-i += 6;
+else
+{
+return (is_prime_number_recursion(n, 2));
 }
-return (1);
 }
